3e9a1160cd69119d4345c191d2ba6c696cb686d5
Harshul Sharma <harshulsharma000@outlook.com>
2017-10-17 14:52:01 +0530

    A reusable navigation boilerplate on its way

---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+import Welcome from '../screens/Welcome';
+import Authen from '../screens/Authen';
+import Mainscreen from '../screens/Welcome';
+
+const Routes = {
+    Welcome: { screen: Welcome },
+    Auth: { screen: Authen },
+    Main: { screen: Mainscreen }
+    };
+
+export default Routes;
